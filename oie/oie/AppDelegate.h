297434b09253e7d424c190062b199fc6cd6fef9c
Bruno Fernandes <me@brunofernand.es>
2014-01-13 20:32:42 +0100

    Now with 100% more .h and .m
    Hello world!

---
+++ oie/oie/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  oie
+//
+//  Created by Bruno Fernandes on 13/01/14.
+//  Copyright (c) 2014 Paper Street Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ oie/oie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  oie
+//
+//  Created by Bruno Fernandes on 13/01/14.
+//  Copyright (c) 2014 Paper Street Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
