ab490d60655e74bca37ead8f6b23186ab36022b0
Bruno Koga <koga@brunokoga.com>
2014-02-03 11:51:04 -0200

    Adding Third Example

--- Draftpad/AppDelegate.m
+++ Draftpad/AppDelegate.m
@@ -31,6 +31,8 @@
     [self firstExample];
     [self secondExample];
     
+    [self thirdExample];
+    
     return YES;
 }
 
@@ -168,5 +170,82 @@
     NSLog(@"\nArtista: %@\nÁlbum: %@", artistName, albumName);
 }
 
+- (void)thirdExample
+{
+    /*
+     
+     Uma das partes mais importantes da programação - muitas vezes mais importante
+     que o próprio código em si - é a forma como o código é organizado.
+     
+     Existe um princípio chamado Single Responsability Principle - http://en.wikipedia.org/wiki/Single_responsibility_principle
+     que nos fala que, utilizando orientação a objetos, devemos sempre
+     fazer com que nossas classes tenham **uma única responsabilidade**.
+     Em outras palavras, devemos quebrar o máximo possível nosso código em várias classes.
+     Isso facilita manutenção e modularidade, além de ajudar em dois outros conceitos 
+     importantes dentro de OO: alta coesão e baixo acoplamento 
+     - http://en.wikipedia.org/wiki/Cohesion_(computer_science)
+     - http://en.wikipedia.org/wiki/Coupling_(computer_programming)
+     
+     Essas leituras serão um tanto quanto abstratads, mas vai nos ajudar para que, a partir de agora
+     pensemos sempre em como/onde vamos escrever nosso código.
+     
+     Um exemplo bem feito de SOR nesse momento é a nossa classe de DPAlbum, que tem uma única responsabilidade:
+     representar um objeto real no nosso programa (ou seja, é uma entidade).
+     
+     Um exemplo mal feito de SOR no nosso projeto é essa classe aqui (AppDelegate). Afinal, qual é a responsabilidade
+     dessa classe? Segundo a documentação da Apple:
+     
+     "The app delegate is a custom object created at app launch time, usually by the UIApplicationMain function. The primary job of this object is to handle state transitions within the app. For example, this object is responsible for launch-time initialization and handling transitions to and from the background. For information about how you use the app delegate to manage state transitions, see “Managing App State Changes.”
+     In iOS 5 and later, you can use the app delegate to handle other app-related events. The Xcode project templates declare the app delegate as a subclass of UIResponder. If the UIApplication object does not handle an event, it dispatches the event to your app delegate for processing. For more information about the types of events you can handle, see UIResponder Class Reference." - https://developer.apple.com/library/ios/documentation/iphone/conceptual/iphoneosprogrammingguide/AppArchitecture/AppArchitecture.html
+     
+     Logo, o que podemos notar aqui? Que nossos exemplos estão no lugar errado!
+     
+     Exercício: refatorar nosso código para que todos esses exemplos fiquem em classes separadas. Aconselho:
+     
+     1) Criar uma superclasse (Por exemplo: DPExemple) com apenas um método no .h: - (void)runExample. A implementação desse método (.m) deve ser vazia (!!!!),
+     ou seja: assim:
+     
+     - (void)runExample { }
+     
+     2) Criar 3 subclasses (cada uma com seus próprios .h e .m) de DPExemple: DPFirstExample, DPSecondExample, DPThirdExample. Essas subclasses não precisam declarar
+     **nenhum** método no .h, mas devem implementar (.m) o método - (void)runExample. Para isso, copie o conteúdo dos métodos que estão nessa classe aqui
+     para suas respectivas classes. (Lembre-se: os métodos devem todos se chamar -runExample ao invés de -firstExample, -secondExample e -thirdExample). 
+     PS - um dos exemplos vai ter um erro de compilação. Tente entender o erro e a solução.
+     
+     3) Entender o que fizemos aí em cima. Em poucas palavras, nós criamos uma classe abstrata, que define um método mas não o implementa.
+     Cada subclasse dessa classe abstrata, então, implementa o método como achar melhor. Tente fazer outras analogias, como, por exemplo, num joguinho.
+     Imagine que a classe abstrata seja "Personagem" e ela defina o método "-movimentar". Imagine que você tem 3 subclasses: golfinho, pássaro e tartaruga.
+     Cada uma dessas classes implementa o método "-movimentar" de uma forma totalmente diferente, mas para o cara que vai chamar esses métodos, isso fica transparente.
+     Ou seja, no seu programa vc simplesmente vai falar: "[gofinho movimentar]" ou "[pássaro movimentar]". A **responsabilidade** de como esse
+     personagem vai, de fato, executar a tarefa dele é do próprio personagem (objeto) e não de quem chama a tarefa (método).
+     
+     4) Agora podemos deixar nosso AppDelegate bem mais enxuto. Podemos deixar apenas o método "- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions" que é o primeiro método que é chamado após a aplicação ter sido executada.
+     
+     5) Para cada exemplo, queremos fazer o seguinte:
+    
+        DPExample *example1 = [DPFirstExample new];
+        [example1 runExample];
+     
+        Note que:
+        
+        - Apesar de estarmos instanciando um DPFirstExample, estamos atribuindo ele a uma variável do tipo da superclasse dela (no caso, DPExample). 
+        Isso funciona perfeitamente (e é uma boa prática de programação). É o tal do polimorfismo! - http://pt.wikipedia.org/wiki/Polimorfismo
+        - Novamente teremos um erro de compilação. Você já deve saber resolvê-lo :)
+     
+     6) Repita o item 5 para os outros exemplos.
+     
+     7) Note que na nossa classe AppDelegate, temos um #import desnecessário (pois estamos fazendo o #import mas não usamos a classe importada
+     em nenhum lugar. Aqui isso não vai ser um problema, mas em programas maiores, isso pode causar lentidão (principalmente para compilar o programa)
+     e fere os princípios do baixo acoplamento. Remova esse import.
+     
+     8) Voilá! Tudo deve estar funcionando, mais bonito e mais belo! É hora de adicionar os arquivos gerados e modificados no git, commitar com uma
+     frase bem bonita e descritiva e pushar tudo isso para o repositório!
+     
+     */
+    
+    NSLog(@"YAY!");
+    
+    
+}
 
 @end
